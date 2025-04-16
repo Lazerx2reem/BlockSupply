3e4cf36cb6bec0a58e5ef4893ea6f37e5402006f
Arsh Gupta <arsh121523@gmail.com>
2025-04-16 00:15:47 -0400

    Restarted

--- utils/contract.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-import ProductRegistryABI from "./ProductRegistryABI.json";
-
-const CONTRACT_ADDRESS = "YOUR_DEPLOYED_CONTRACT_ADDRESS";
-
-export function getContract(signerOrProvider) {
-  return new ethers.Contract(CONTRACT_ADDRESS, ProductRegistryABI, signerOrProvider);
-}
